678076758293681a2e693c2564a328cb4ea17c07
andrewto30 <andrew.to30@gmail.com>
2024-03-23 16:09:16 -0400

    properly retrieve env variables and served data to /data route

--- flask-backend/resources/example_prompt_one.py
+++ flask-backend/resources/example_prompt_one.py
@@ -1,72 +1,47 @@
-# import mysql.connector
-# from mysql.connector import Error
-# import logging
-
-# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# def connect_fetch_data():
-#     """Connects to MySQL database and fetches data"""
-#     connection = None
-
-#     try:
-#         # Connection parameters
-#         connection = mysql.connector.connect(
-#             host='phillygptdb1.czgma8k2kt6g.us-east-2.rds.amazonaws.com',
-#             database='phillygpt',
-#             user='admin',
-#             password='vuwva8-vZgabt-zip5m'
-            
-#         )
-#         logging.info("Setting MySql database connection parameters...")
-
-#         if connection.is_connected():
-#             db_info = connection.get_server_info()
-#             logging.info("Connected to MySql Server version: %s", db_info)
-#             cursor = connection.cursor()
-#             cursor.execute("SELECT * FROM Bike_Network LIMIT 10;")
-#             records = cursor.fetchall()
-            
-#             for row in records:
-#                 logging.info("Fetched data: %s", row)
-#                 #print(row)
-
-#     except Error as e:
-#         logging.warning("Error connecting to MySql Database: %s", e)
-
-#     finally:
-#         if connection is not None and connection.is_connected():
-#             cursor.close()
-#             connection.close()
-#             logging.info("MySql Connection closed.")
-
-# if __name__ == '__main__':
-#     connect_fetch_data()
-
+from flask import Flask, jsonify
 import mysql.connector
 from mysql.connector import Error
 from sshtunnel import SSHTunnelForwarder
 import logging
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+app = Flask(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+print(os.environ.get("SSH_PRIVATE_KEY"))
 
 def connect_fetch_data():
     """Connects to MySQL database through SSH tunnel and fetches data"""
     tunnel = None
     connection = None
+    data = []
 
     try:
-        # SSH tunnel parameters
-        ssh_host = 'ec2-3-13-209-241.us-east-2.compute.amazonaws.com'
-        ssh_port = 22
-        ssh_username = 'ec2-user'
-        ssh_private_key = r'C:\Users\quynh\.ssh\phillygpt.pem'
+        # # SSH tunnel parameters
+        # ssh_host = 'ec2-3-13-209-241.us-east-2.compute.amazonaws.com'
+        # ssh_port = 22
+        # ssh_username = 'ec2-user'
+        # ssh_private_key = r'C:\Users\andrewto\.ssh\phillygpt.pem'
 
-        # MySQL database parameters
-        mysql_host = 'phillygptdb1.czgma8k2kt6g.us-east-2.rds.amazonaws.com'
-        mysql_port = 3306
-        mysql_username = 'admin'
-        mysql_password = 'vuwva8-vZgabt-zip5m'
-        mysql_db = 'phillygpt'
+        # # MySQL database parameters
+        # mysql_host = 'phillygptdb1.czgma8k2kt6g.us-east-2.rds.amazonaws.com'
+        # mysql_port = 3306
+        # mysql_username = 'admin'
+        # mysql_password = 'vuwva8-vZgabt-zip5m'
+        # mysql_db = 'phillygpt'
+
+        # SSH tunnel parameters
+        ssh_host = os.getenv('SSH_HOST')
+        ssh_port = int(os.getenv('SSH_PORT', 22))  # Default to port 22 if not specified
+        ssh_username = os.getenv('SSH_USERNAME')
+        ssh_private_key = os.getenv('SSH_PRIVATE_KEY')
+
+        mysql_host = os.getenv('MYSQL_HOST')
+        mysql_port = int(os.getenv('MYSQL_PORT', 3306))  # Default to port 3306 if not specified
+        mysql_username = os.getenv('MYSQL_USERNAME')
+        mysql_password = os.getenv('MYSQL_PASSWORD')
+        mysql_db = os.getenv('MYSQL_DB')
 
         # Establish SSH tunnel
         tunnel = SSHTunnelForwarder(
@@ -92,10 +67,11 @@ def connect_fetch_data():
             db_info = connection.get_server_info()
             logging.info("Connected to MySql Server version: %s", db_info)
             cursor = connection.cursor()
-            cursor.execute("SELECT * FROM Bike_Network LIMIT 100;")
+            cursor.execute("SELECT * FROM farmers_markets_location LIMIT 100;")
             records = cursor.fetchall()
 
             for row in records:
+                data.append(row[:6])
                 logging.info("Fetched data: %s", row)
 
     except Error as e:
@@ -110,6 +86,13 @@ def connect_fetch_data():
         if tunnel is not None and tunnel.is_active:
             tunnel.stop()
             logging.info("SSH tunnel closed.")
+    
+    return data
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    data = connect_fetch_data()
+    return jsonify(data)
 
 if __name__ == '__main__':
-    connect_fetch_data()
+    app.run(debug=True)
