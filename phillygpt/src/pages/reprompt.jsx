497e02e10f2a45a271a614ca6cc92b98f2ef666e
zyij99 <bzbrandon1999@gmail.com>
2024-03-29 13:09:35 -0400

    updated requirements.txt and removed display area in reprompt (replaced w/ comment.)

--- phillygpt/src/pages/reprompt.jsx
+++ phillygpt/src/pages/reprompt.jsx
@@ -16,9 +16,8 @@ const RepromptPage = () => {
       <SearchBar/>
       <div className="suggestion-prompt-container">
         <div className={`text-${isDark ? 'white' : 'black'} text-center mb-4 font-lightbold`}> We're unable to find what you're looking for, here are some suggestions to improve your prompt. </div>
-        <DisplayArea/> {/* REPLACE WITH SUGGESTIONS*/}
+        {/* REPLACE WITH SUGGESTIONS*/}
       </div>
-
     </div>
   );
 }
