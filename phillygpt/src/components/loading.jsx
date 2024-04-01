08a3782f0f3a0f9e412747a7216b26174355da94
zyij99 <bzbrandon1999@gmail.com>
2024-03-31 21:47:39 -0400

    added loading icon component

---
+++ phillygpt/src/components/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SpinningCircles} from 'react-loading-icons'
+const Loading = () => {
+    return(
+        <div className="loading-container">
+            <SpinningCircles/>
+        </div>
+    )
+}
+
+export default Loading;
