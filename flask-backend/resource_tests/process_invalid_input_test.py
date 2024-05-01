99c3eaeae2c4a032cd202e8999791a7ddc20b336
Aidan Scharnikow <tup95685@temple.edu>
2024-05-01 17:33:36 -0400

    Added unit test and proper testing report output.

--- flask-backend/resource_tests/process_invalid_input_test.py
+++ flask-backend/resource_tests/process_invalid_input_test.py
@@ -4,11 +4,12 @@ def test_post_user_input(client):
     """
     Test the post method of the ProcessInput resource for successful processing of user input.
     """
-    
-    data = {"user_input": "This is a user input"}
+
+    data = {"user_input": "Give me information on the moon."}
     response = client.post("/process_input", json=data)
     assert response.status_code == 200
     json_data = response.get_json()
 
     assert "message" in json_data
     assert "status" in json_data
+    assert json_data["status"] == "error"
